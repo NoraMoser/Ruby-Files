5564c9c117ce653b3b2230f4a190d8bcc2dc553d
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-10-12 16:20:31 -0500

    getting there

--- file_io.rb
+++ file_io.rb
@@ -1,17 +1,32 @@
 require 'yaml'
 
 f1 = File.open("car_makes.txt", "r+")
-puts f1.read
+# puts f1.read
 f2 = File.open("car_models.txt", "r+")
-puts f2.read
+# puts f2.read
 
 hash = {}
-File.open("car_models.txt") do |ok|
-  hash.push(ok)
-  File.open("car_makes.txt") do |dunno|
-    hash.push(dunno)
-  end
+array = []
+array2 = []
+array_of_arrays = []
+
+
+
+f2.each do |models|
+    array = models.split("=")
+    array_of_arrays.push(array[1])
+end
+
+f1.each do |makes|
+    array_of_arrays.push(makes)
+    # if makes.firstletter = array_of_arrays[0]
 end
 
 
-puts hash
+
+
+puts array_of_arrays
+
+
+# split the car_models at the = 
+# and then .each through makes first see if == index[0] model and assign to hash (hash[make = model])
