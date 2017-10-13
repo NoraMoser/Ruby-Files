a75c9f0229a626c5d60a80da6b75224b15f5b38f
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2017-10-12 20:07:26 -0500

    mostly working

--- file_io.rb
+++ file_io.rb
@@ -5,27 +5,35 @@ f1 = File.open("car_makes.txt", "r+")
 f2 = File.open("car_models.txt", "r+")
 # puts f2.read
 
-hash = {}
+hash = Hash.new
 array = []
 array2 = []
-array_of_arrays = []
 
 
 
 f2.each do |models|
     array = models.split("=")
-    array_of_arrays.push(array[1])
+    # puts array
+    f1.each do |makes|
+        array2 = makes[0]
+        # puts array2
+        # if array[0] == array2
+            hash[makes] = [array[1]]
+        # end
+    end
+    # array_of_arrays.push(array[1])
 end
 
-f1.each do |makes|
-    array_of_arrays.push(makes)
-    # if makes.firstletter = array_of_arrays[0]
-end
+puts hash
+# f1.each do |makes|
+#     array_of_arrays.push(makes)
+#     # if makes.firstletter = array_of_arrays[0]
+# end
 
 
 
 
-puts array_of_arrays
+# puts array_of_arrays
 
 
 # split the car_models at the = 
